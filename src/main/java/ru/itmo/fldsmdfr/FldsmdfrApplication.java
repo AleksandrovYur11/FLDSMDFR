932b78ac42cbc926dc1f271c35734c9d60f7d1ac
AleksandrovYur11 <yurochka.aleksandrov.99@mail.ru>
2023-05-21 18:19:06 +0300

    Initial commit

---
+++ src/main/java/ru/itmo/fldsmdfr/FldsmdfrApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.fldsmdfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FldsmdfrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FldsmdfrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/fldsmdfr/FldsmdfrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.fldsmdfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FldsmdfrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
