3d4630b4b0c9f2723599b3a94383dfd1f9030e00
hyperbot <vitalij-groshev@mail.ru>
2023-06-03 22:00:48 +0300

    add userRepository

--- src/main/java/ru/itmo/fldsmdfr/models/User.java
+++ src/main/java/ru/itmo/fldsmdfr/models/User.java
@@ -1,9 +1,13 @@
 package ru.itmo.fldsmdfr.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "user", schema = "public")
+@Getter
+@Setter
 public class User {
 
     @Id
