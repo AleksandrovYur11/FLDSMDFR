f4f686519e2a6c5b9b31ad811370492ff9efb737
hyperbot <vitalij-groshev@mail.ru>
2023-06-14 04:11:59 +0300

    add missing files for device mock service

---
+++ fldsmdfr-mock/src/main/java/ru/itmo/fldsmdfrmock/FldsmdfrMockApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.fldsmdfrmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FldsmdfrMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FldsmdfrMockApplication.class, args);
+	}
+
+}
---
+++ fldsmdfr-mock/src/test/java/ru/itmo/fldsmdfrmock/FldsmdfrMockApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.fldsmdfrmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FldsmdfrMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
